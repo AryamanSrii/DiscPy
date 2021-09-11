c4bf202a7f4a0477b9edf952ebf3d3b76dce250e
sn1f3rt <sn1f3rt@outlook.com>
2021-09-11 21:14:28 +0530

    Fix a setup issue

--- setup.py
+++ setup.py
@@ -12,30 +12,6 @@ with open("discpy/__init__.py") as f:
 if not version:
     raise RuntimeError("version is not set")
 
-if version.endswith(("a", "b", "rc")):
-    # append version identifier based on commit count
-    try:
-        import subprocess
-
-        p = subprocess.Popen(
-            ["git", "rev-list", "--count", "HEAD"],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-        )
-        out, err = p.communicate()
-        if out:
-            version += out.decode("utf-8").strip()
-        p = subprocess.Popen(
-            ["git", "rev-parse", "--short", "HEAD"],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-        )
-        out, err = p.communicate()
-        if out:
-            version += "+g" + out.decode("utf-8").strip()
-    except Exception:
-        pass
-
 with open("README.md") as f:
     readme = f.read()
 
